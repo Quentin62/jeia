d2dbd73fc530a82b1ff56701ba1966c59aa12fab
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2019-03-02 14:23:36 +0100

    use shinydashboard

--- ui.R
+++ ui.R
@@ -1,25 +1,37 @@
 library(shiny)
 library(shinyjs)
+library(shinydashboard)
 library(DT)
 
-fluidPage(
-  useShinyjs(),
-  tabsetPanel(
-    tabPanel("Données",
-             br(),
-             column(6, offset = 3, dataTableOutput("donnees"))
-    ),
-    tabPanel("Visualisation", 
-             br(),
-             sidebarLayout(
-               sidebarPanel(selectInput("selectedVar", choices = c("Height", "Weight"), label = NULL),
-                            uiOutput("slider")),
-               mainPanel(plotOutput("distPlot"),
-                         actionButton("show", "Montrer les paramètres optimaux"),
-                         br(), br(),
-                         hidden(uiOutput("param"))
-               )
-             )
-    )
-  )
+dashboardPage(skin = "red",
+              dashboardHeader(title = "JEIA 2019"),
+              
+              dashboardSidebar(
+                sidebarMenu(id = "tabs",
+                            menuItem("Taille et poids", tabName = "taille", icon = icon("ruler-vertical")))),
+              dashboardBody(
+                useShinyjs(),
+                tabItems(
+                  tabItem(tabName = "taille", 
+                          tabsetPanel(
+                            tabPanel("Données",
+                                     br(),
+                                     column(6, offset = 3, dataTableOutput("donnees"))
+                            ),
+                            tabPanel("Visualisation", 
+                                     br(),
+                                     sidebarLayout(
+                                       sidebarPanel(selectInput("selectedVar", choices = c("Height", "Weight"), label = NULL),
+                                                    uiOutput("slider")),
+                                       mainPanel(plotOutput("distPlot"),
+                                                 actionButton("show", "Montrer les paramètres optimaux"),
+                                                 br(), br(),
+                                                 hidden(uiOutput("param"))
+                                       )
+                                     )
+                            )
+                          )
+                  )
+                )
+              )
 )
